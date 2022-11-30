468a815ef8697b638946be3c76694ebd2b780001
Mugeon Kim <103854287+KMGeon@users.noreply.github.com>
2022-11-30 18:55:54 +0900

    Add files via upload

---
+++ FPS/PlayerHealth.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    [SerializeField] float hitpoints = 100f;
+    public void TakeDamage(float damage)
+    {
+        hitpoints -= damage;
+        if (hitpoints <= 0)
+        {
+            GetComponent<DeathHandler>().HandleDeath();
+        }
+
+    }
+}
